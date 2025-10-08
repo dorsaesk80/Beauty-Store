867aaa732d8bae25b9ecee88655baffd12c73606
dorsaesk80 <dorsa.esk80@gmail.com>
2025-10-08 13:08:25 +0330

    worked on product model and controller

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -70,5 +70,6 @@ const ProductSchema = mongoose.Schema({
   ],
 });
 
+ProductSchema.index({ "$**": "text" });
 const Product = mongoose.model("Product", ProductSchema);
 export default Product;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -39,6 +40,20 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+userSchema.methods.matchPassword = async function(eneteredPassword){
+return await bcrypt.compare(eneteredPassword, this.password);
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
