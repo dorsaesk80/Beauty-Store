eaf0ab7e1921d686a4aec36f44d65a1bbcc956dc
dorsaesk80 <dorsa.esk80@gmail.com>
2025-10-17 20:05:41 +0330

    finished working on bservices

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -4,6 +4,7 @@ import dbConnection from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js";
+import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
 dotenv.config();
 
 const app = express();
@@ -14,13 +15,22 @@ const services = () => {
   cron.schedule("* * * * * *", () => {
     sendWelcomeEmail();
     sendPendingOrderEmail();
+    sendDeliveredOrderEmail();
+  });
+};
+
+
+const promotion = () => {
+  cron.schedule("30 5 * * 5", () => {
+    sendPromotionEmail();
   });
 };
 
 services();
+promotion();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Backgroundservices is running on port ${PORT}`);
   dbConnection();
 });
