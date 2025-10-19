800859082b54b3c365d028535070a7ebd3ab14c2
dorsaesk80 <dorsa.esk80@gmail.com>
2025-10-19 15:56:37 +0330

    started frontend

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -5,6 +5,8 @@ import cron from "node-cron";
 import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
 import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js";
 import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
+import sendPromotionEmail from "./EmailServices/sendPromotionemail.js";
+
 dotenv.config();
 
 const app = express();
