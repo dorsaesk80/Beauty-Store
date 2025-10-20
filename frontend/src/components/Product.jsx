4fc33dfcd29f2492e6051fb3a4dc25c8ed4e119b
dorsaesk80 <dorsa.esk80@gmail.com>
2025-10-20 20:56:00 +0330

    completed homepage

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react"; 
+import Rating from "@mui/material/Rating";
+
+const Product = () => {
+    const [value, setValue] = useState(2.5); 
+  return (
+    <div className="flex flex-col items-center justify-center h-[500px] m-[10px] cursor-pointer">
+        <img src="/serum1.jpg" alt="" className="h-[400px] w-[300px] bg-cover" />
+        <h2 className="font-semibold text-[18px] w-[300px]">
+          Rosehip Seed ,Argan ,Sweet Almond & Vitamin E Oil-Anti-aging
+        </h2>
+        <span className="text-[18px] font-semibold flex items-center justify-center">
+          $100
+        </span>
+
+        <span className="flex items-center">
+            <Rating 
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+        (2)
+        </span>
+        
+      </div>
+  )
+}
+
+export default Product
