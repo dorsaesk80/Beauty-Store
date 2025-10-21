8ff770dd2961153cc766a97785745aa2477f6839
dorsaesk80 <dorsa.esk80@gmail.com>
2025-10-21 13:17:28 +0330

    worked on product detailed page

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import Rating from "@mui/material/Rating";
 
-const Product = () => {
-    const [value, setValue] = useState(2.5); 
+const Product = ({ img }) => {
+  const [value, setValue] = useState(2.403);
   return (
-    <div className="flex flex-col items-center justify-center h-[500px] m-[10px] cursor-pointer">
-        <img src="/serum1.jpg" alt="" className="h-[400px] w-[300px] bg-cover" />
-        <h2 className="font-semibold text-[18px] w-[300px]">
-          Rosehip Seed ,Argan ,Sweet Almond & Vitamin E Oil-Anti-aging
-        </h2>
-        <span className="text-[18px] font-semibold flex items-center justify-center">
-          $100
-        </span>
+    <div className="flex flex-col items-center justify-center h-[500px] m-[30px] cursor-pointer">
+      <img src={img} alt="" className="h-[400px] w-[300px] bg-cover" />
+      <h2 className="font-semibold text-[18px] w-[300px]">
+        Rosehip Seed ,Argan ,Sweet Almond & Vitamin E Oil-Anti-aging
+      </h2>
+      <span className="text-[18px] font-semibold flex items-center justify-center">
+        $100
+      </span>
 
-        <span className="flex items-center">
-            <Rating 
+      <span className="flex items-center">
+        <Rating
           name="simple-controlled"
           value={value}
           onChange={(event, newValue) => {
@@ -22,10 +22,9 @@ const Product = () => {
           }}
         />
         (2)
-        </span>
-        
-      </div>
-  )
-}
+      </span>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
